1bb3cf5d9d1fed5ee1cb8b2969345b6118438fd1
heju <heju@sobey.com>
2017-07-12 19:50:44 +0800

    提交测试代码

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,16 @@
  */
 
 class Main {
-    constructor(){
-        console.log("123");
+    constructor(eleId){
+        var canvasDom = document.getElementById(eleId);
+        var canvas = document.createElement("CANVAS");
+        canvas.width = canvasDom.offsetWidth;
+        canvas.height = canvasDom.offsetHeight;
+        canvasDom.appendChild(canvas);
+        var ctx = canvas.getContext('2d');
+        ctx.strokeStyle = "#000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
+
+window.Monk = Main;
