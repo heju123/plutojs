e5a86350f35ad0d2fde3c5b94f6803b5ea3d87b8
heju <heju@sobey.com>
2018-01-16 14:52:05 +0800

    提取manifest和vendor文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 function resolve (dir) {
     return path.join(__dirname, '.', dir)
@@ -8,21 +9,21 @@ module.exports = function(env){
     var cleanWebpackPlugin = require('clean-webpack-plugin');
 
     var devtool = "source-map";
-    var plugins = [];
-    plugins.push(new cleanWebpackPlugin(['**'],
-        {
-            root: 'D:\\nodeProjects\\plutoTest\\node_modules\\plutojs',
-            verbose: true,
-            dry: false
-        }));
 
     return {
-        entry: __dirname + '/test/src/js/main.js',
+        entry: {
+            vendor: [
+                "./libs/TweenLite/TweenMax.min.js",
+                "./libs/TweenLite/easing/EasePack.min.js",
+                "./libs/TweenLite/plugins/ColorPropsPlugin.min.js"
+            ],
+            app: __dirname + '/test/src/js/main.js'
+        },
         output: {
             path: __dirname + '/test/build',
             publicPath : "/build/",
-            filename: "bundle.js",
-            chunkFilename: '[name].chunk.js'
+            filename: "[name].js",
+            chunkFilename: '[name].[chunkhash:5].chunk.js'
         },
         devtool: devtool,  //生成source file
         resolve: {
@@ -37,6 +38,26 @@ module.exports = function(env){
                 }
             ]
         },
-        plugins: plugins
+        plugins: [
+            new cleanWebpackPlugin(['test/build/**'],
+                {
+                    root: '',
+                    verbose: true,
+                    dry: false
+                }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "vendor",
+                // filename: "vendor.js"
+                // (Give the chunk a different name)
+
+                minChunks: Infinity,
+                // (with more entries, this ensures that no other module
+                //  goes into the vendor chunk)
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'manifest',
+                minChunks: Infinity
+            })
+        ]
     };
 };
